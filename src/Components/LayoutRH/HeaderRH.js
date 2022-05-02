a28996d50581eb149b8f84d809890f06daf927b1
DaniloScalzo <danilosfaro@gmail.com>
2022-05-02 19:02:49 -0300

    -Criação do Escopo do projeto

---
+++ src/Components/LayoutRH/HeaderRH.js
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <div>
+            <Link to="">Home</Link>
+            <Link to="">Registro de Atividades</Link>
+            <Link to="">Instituições</Link>
+            <Link to="">Formulários</Link>
+        </div>
+    )
+}
+
+export default Navbar
