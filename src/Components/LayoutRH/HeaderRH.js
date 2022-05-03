50fe19639c6254bcc6749fd2c07aed2b18680920
DaniloScalzo <danilosfaro@gmail.com>
2022-05-02 21:56:24 -0300

    Adição no navbar de RH

--- src/Components/LayoutRH/HeaderRH.js
+++ src/Components/LayoutRH/HeaderRH.js
@@ -5,6 +5,8 @@ function Navbar() {
             <Link to="">Registro de Atividades</Link>
             <Link to="">Instituições</Link>
             <Link to="">Formulários</Link>
+            <Link to="">Solicitações</Link>
+            <Link to="">Registro de Formulários</Link>
         </div>
     )
 }
