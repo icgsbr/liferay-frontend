8753a7f864b77f43303ff803fb7ac81dc28a003e
gabrielb <gabriel.silva900@gmail.com>
2022-05-29 14:04:29 -0300

    ajuste no titleInfoGlobal

---
+++ src/Infos/solicitation-info.js
@@ -0,0 +1,18 @@
+const solicitationInfo = [
+    {
+    id:1,
+    nome: "Maria",
+    setor: Administrativo,
+    tipo: "doação",
+    valor:30.00
+    },
+    {
+    id:2,
+    nome: "João",
+    setor: Administrativo,
+    tipo: "doação",
+    valor:60.00
+    }
+
+
+]
