9c53f029ea5a945726d1927063fddf567d256d1d
gabrielb <gabriel.silva900@gmail.com>
2022-06-05 15:52:38 -0300

    criação da page solicitações RH

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Home from "./Pages/User/Home";
 import RegisterActivity from "./Pages/User/RegisterActivity";
 import Institution from "./Pages/User/Institution";
 import AddInstitution from "./Pages/User/AddInstitution"
-import InstitutionSelect from "./Pages/User/InstitutionSelect"
 import Forms from "./Pages/User/Forms"
 import HomeRH from "./Pages/RH/HomeRH"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { ModelContext } from "./Components/ModelContext";
 import HomeDetail from "./Pages/User/HomeDetail";
+import Solicitations from "./Pages/RH/Solicitations";
 import RegisterActivityDetail from "./Pages/User/RegisterActivityDetail";
 import InstitutionDetail from "./Pages/User/InstitutionDetail";
 import FormActivity from "./Pages/User/FormActivity";
@@ -35,9 +35,9 @@ function App() {
                             <Route path="/registeractivity" element={<RegisterActivity/>}/>
                             <Route path="/institution" element={<Institution/>}/>
                             <Route path="/addInstitution" element={<AddInstitution/>}/>
-                            <Route path="/institutionSelect" element={<InstitutionSelect/>}/>
                             <Route path="/homedetail" element={<HomeDetail/>}/>
                             <Route path="/registeractivitydetail" element={<RegisterActivityDetail/>}/>
+                            <Route path="/solicitations" element={<Solicitations/>}/>
                             <Route path="/institutiondetail" element={<InstitutionDetail/>}/>
                             <Route path="/formactivity" element={<FormActivity/>}/>
                         </Routes>
--- src/Pages/User/Institution.js
+++ src/Pages/User/Institution.js
@@ -47,7 +47,7 @@ function Institution() {
                             </select>
                             <span>filtro</span>
                             <button className="btn btn-primary" id="btn-filter-institution">Minhas Instituições</button>
-                                </div>
+                        </div>
                         <div className="institution-card-container">
                             {institutionInfo.map((info) =>
                                 <InstitutionCard
