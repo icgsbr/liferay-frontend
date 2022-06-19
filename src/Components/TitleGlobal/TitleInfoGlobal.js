44ed4cf31a4fc05db7593ad986f7eb42f5550289
gabrielb <gabriel.silva900@gmail.com>
2022-06-19 16:50:53 -0300

    ajuste no title global info

--- src/Components/TitleGlobal/TitleInfoGlobal.js
+++ src/Components/TitleGlobal/TitleInfoGlobal.js
@@ -13,14 +13,14 @@ function TitleInfoGlobal(props) {
             </div>
             <div className="col info-global-container">
                 <div>
-                    <span className="title-info-global info-global-style">{props.titledonateglobal}</span>
+                    <span className="title-info-global info-global-style">{props.titledonateglobal}</span><br></br>
                     <span className="info-global-style">{props.titledonatevalor}</span>
                     <span className="info-global info-global-style">{props.donatevalue}</span>
                     <span className="info-global-style">{props.titledonatemeta}</span>
                     <span className="info-global info-global-style">{props.donatemeta}</span>
                 </div>
                 <div>
-                    <span className="title-info-global info-global-style">{props.titleactivityglobal}</span>
+                    <span className="title-info-global info-global-style">{props.titleactivityglobal}</span><br></br>
                     <span className="info-global-style">{props.titleactivityvalor}</span>
                     <span className="info-global info-global-style">{props.activityvalue}</span>
                     <span className="info-global-style">{props.titleactivitymeta}</span>
